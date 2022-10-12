e649eb86abd090c45eb4e303b0a0c5daac1203ca
“远道 <>
2022-10-12 17:27:49 +0800

    修复版本

--- config.php
+++ config.php
@@ -2,8 +2,6 @@
 /**
  * XDING WeChat Public Platform
  * 微信公众号 PHP 版推送信息
- *作者QQ:1825703954
- * @copyright  Copyright (c) 2022 XDING  (https://wxnljun.asia)
  * @page      配置页
  */
 
@@ -16,16 +14,15 @@ $WXconfig=array(
 	'app_id' => '', //公众号appId
 	'app_secret' => '', //公众号appSecret
 	'template_id' => '', //模板消息id
-	'user' => array('') //接收公众号消息的微信号，如果有多个，需要在()里用英文逗号间隔，例如('君临','小君')
+	'user' => array('') //接收公众号消息的微信号，如果有多个，需要在()里用英文逗号间隔，例如('小明','小丁')
 );
-
 /** 微信程序信息配置 */
 $INFOconfig=array(
-	'region' => '', //所在地区，可为城市，区，县，同时支持国外城市，例如伦敦
-	'birthday1' => array('name'=>'名字','birthday'=>'日期'), //修改名字为对应需要显示的名字，生日为公历哦~ 格式：2022-8-3
-	'birthday2' => array('name'=>'','birthday'=>''), //同上
-	'love_date'=>'', //同上
-	"note_ch"=>'早~宝贝，14亿人我就通知了你，所以请在新的一天里开开心心！快快乐乐！顺顺利利哦~ 爱你', // 金句中文，如果设置了，则会显示这里的。
-	"note_en"=>'I never stopped loving you, I just unshowing it.' //金句英文
+	'region' => '武汉', //所在地区，可为城市，区，县，同时支持国外城市，例如伦敦
+	'birthday1' => array('name'=>'菁菁','birthday'=>'2000-01-01','region'=>'台湾'), //修改名字为对应需要显示的名字，生日为公历哦~ 格式：2022-8-3
+	'birthday2' => array('name'=>'阿羡','birthday'=>'2000-01-01','region'=>'香港'), //同上
+	'love_date'=>'2022-10-12', //同上
+	"note_ch"=>'', // 金句中文，如果设置了，则会显示这里的，如果为空，默认会读取金山的每日金句
+	"note_en"=>'' //金句英文
 );
 
--- index.php
+++ index.php
@@ -2,14 +2,17 @@
 /**
  * XDING WeChat Public Platform
  * 微信公众号 PHP 版推送信息
- *作者QQ:1825703954
- * @copyright  Copyright (c) 2022 XDING  (https://wxnljun.asia)
+ *
+ * @copyright  Copyright (c) 2022 XDING  (https://i4coder.com)
+ 
  * @page      Main页
  */
 require './config.php';
 $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$WXconfig['app_id']."&secret=".$WXconfig['app_secret'];
-$returnJson = json_decode(Request_curl($url,''),true);
-$token = $returnJson["access_token"];
+$returnJson = file_get_contents($url); //get请求获取
+$arr = json_decode($returnJson,true); //转数组
+$token = $arr["access_token"]; //取token
+
 if(!$token){
 	$return = array(
 		"code"=> $returnJson['errcode'],
@@ -19,8 +22,8 @@ if(!$token){
 }
 
 $lunar=new Lunar();
-$today=$lunar->convertSolarToLunar(date(Y),date(m),date(d));
-$month=$lunar->getJieQi(date(Y),date(m),date(d));//获取当前节气
+$today=$lunar->convertSolarToLunar(date('Y'),date('m'),date('d'));
+$month=$lunar->getJieQi(date('Y'),date('m'),date('d'));//获取当前节气
 
 $url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=".$INFOconfig['region']."&needMoreData=false&pageNo=1&pageSize=1";
 $weather = json_decode(file_get_contents($url,true),true);
@@ -33,14 +36,17 @@ if($weather["code"]!="0"){
 	exit(Json($return));
 }
 
+$love = file_get_contents("https://api.vvhan.com/api/love");
+$sjsj = file_get_contents("https://api.vvhan.com/api/love");
+$chp = file_get_contents("https://apibug.cn/api/chp/&apiKey=4149df55b24e216ef55035c73162a78e");
 $url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=".$token;
 $posts = array(
 	"template_id"=>$WXconfig['template_id'],
-	"url"=>"http://wxnljun.asia",
+	"url"=>"http://www.cnhack6.com",
 	"topcolor"=>"#FF0000",
 	"data"=>array(
 		"date"=>array(
-			"value"=> "今天是".date("Y年m月d日")." 农历".$today[1].$today[2]." ".$month[name2]."哦~",
+			"value"=> "今天是".date("Y年m月d日")." 农历".$today[1].$today[2]." ".$month["name2"]."哦~",
 			"color"=>random_color()
 		),
 		"region"=>array(
@@ -71,12 +77,16 @@ $posts = array(
 			"value"=>PassTime($INFOconfig['love_date']),
 			"color"=>random_color()
 		),
+		"chp"=>array(
+			"value"=>$chp,
+			"color"=>random_color()
+		),
 		"note_en"=>array(
-			"value"=>$INFOconfig['note_en'],
+			"value"=>$sjsj,
 			"color"=>random_color()
 		),
 		"note_ch"=>array(
-			"value"=>$INFOconfig['note_ch'],
+			"value"=>$love,
 			"color"=>random_color()
 		),
 	),
@@ -85,20 +95,53 @@ $posts = array(
 for ($catOid = 0; $catOid < count($WXconfig['user']); $catOid++) {
 	$posts["touser"] = $WXconfig['user'][$catOid];
 	$post = json_encode($posts, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    $returnJson = json_decode(Request_curl($url,$post),true);
-    if($returnJson['errcode']=='0'){
-		$return = array(
-			"code"=> 0,
-			"msg"=> "取码网提示：消息推送成功 共".count($WXconfig['user'])."条",
-		);
-	}else{
-		$return = array(
-			"code"=> $returnJson['errcode'],
-			"msg"=> "取码网提示：第".$catOid."条消息推送失败",
-		);
-		exit(Json($return));
-	}
-	//usleep(500000);
+	// echo $post;
+	// return;
+	$returnJson = json_decode(request($url,true,'post',$post),true); //post请求
+	    if($returnJson['errcode']=='0'){
+			$return = array(
+				"code"=> 0,
+				"msg"=> "消息推送成功 共".count($WXconfig['user'])."条",
+			);
+		}else{
+			$return = array(
+				"code"=> $returnJson['errcode'],
+				"msg"=> "第".$catOid."条消息推送失败",
+			);
+			exit(Json($return));
+		}
+	
+	// $return = array(
+	// 	"code"=> 200,
+	// 	"msg"=> '前端执行post请求',
+	// 	"data" => $post,
+	// 	"url" => $url
+	// );
+ 
 }
-
+// 封装request curl方法发送post请求
+function request($url,$https,$method,$data){
+	  $ch = curl_init($url);//1.初始化url
+	  //2.设置请求参数
+	  //把数据以文件流形式保存，而不是直接输出
+	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	  //支持http和https协议
+	  //https协议  ssl证书
+	  //绕过证书验证
+	  if($https === true){
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	  }
+	  
+	  if($method === 'post'){
+	    curl_setopt($ch, CURLOPT_POST, true);
+	    //发送的post数据
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	  }
+	  //3.发送请求
+	  $content = curl_exec($ch);
+	  //4.关闭请求
+	  curl_close($ch);
+	  return $content;
+	}
 exit(Json($return));
